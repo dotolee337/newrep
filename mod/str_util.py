5e06c21b57d44f83b5fcbb2a6a046bb86f3da6a2
dotolee337 <dotolee337@gmail.com>
2023-09-22 18:09:05 +0900

    first commit

--- mod/str_util.py
+++ mod/str_util.py
@@ -3,3 +3,33 @@ def cutstr(st,wd,idx) :
     tmp = st.split(wd)
     res = tmp[idx]
     return res
+
+import math
+def mt_sqrt(x):
+    return math.sqrt(x)
+
+def mt_sinpi():
+    return math.sin(math.pi /2)
+
+def mt_elog():
+    return math.log(math.e)
+
+def mt_sxp(x):
+    return math.exp(x)
+
+def mt_pi():
+    return math.pi
+
+import random as rd
+
+def rd_int(x,y) :
+    return rd.randint(x,y)
+
+def rd_list(this) :
+    return rd.choice(this)
+
+def rd_rd():
+    return rd.random()
+
+def rd_nmvar() :
+    return rd.normalvariate(0,1)
